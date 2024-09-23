04fce1bcf3614e6aaed62978bed3e3a4b0225967
maratreason <maratreason@mail.ru>
2024-09-23 15:04:15 +0300

    chore(prettier) add formatting

--- apps/tafest/src/app/app.component.ts
+++ apps/tafest/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {Component} from "@angular/core";
+import {RouterModule} from "@angular/router";
+import {HeaderComponent, FooterComponent} from "@tafest/layout"
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [RouterModule, HeaderComponent, FooterComponent],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'tafest';
+  title = "tafest";
 }
