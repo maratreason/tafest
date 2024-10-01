6ba13d1054e5aeb0adbe0c988b4aac1ba9652938
maratreason <maratreason@mail.ru>
2024-10-01 15:11:57 +0300

    feat(layout) generate layout library

--- apps/rooi/src/app/app.routes.ts
+++ apps/rooi/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import {Route} from "@angular/router";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {path: "", redirectTo: "/main", pathMatch: "full"},
+    {path: "main", loadComponent: () => import("@rooi").then(c => c.MainComponent)}
+];
