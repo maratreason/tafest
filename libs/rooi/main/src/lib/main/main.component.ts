a36e04d27ca4debe9a5bd78e0fcb37bc5cfa58ea
maratreason <maratreason@mail.ru>
2024-10-08 16:02:01 +0300

    fix(main) add users-list-container

--- libs/rooi/main/src/lib/main/main.component.ts
+++ libs/rooi/main/src/lib/main/main.component.ts
@@ -1,10 +1,11 @@
 import {ChangeDetectionStrategy, Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {UsersListContainerComponent} from "@rooi/users/feature-users-list";
 
 @Component({
     selector: "lib-main",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, UsersListContainerComponent],
     templateUrl: "./main.component.html",
     styleUrl: "./main.component.scss",
     changeDetection: ChangeDetectionStrategy.OnPush,
