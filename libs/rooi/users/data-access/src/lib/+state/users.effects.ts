edb61f865fe684ebc458d2eeaac284e73bdf108e
maratreason <maratreason@mail.ru>
2024-10-08 13:26:15 +0300

    feat(users) add userEffects method

--- libs/rooi/users/data-access/src/lib/+state/users.effects.ts
+++ libs/rooi/users/data-access/src/lib/+state/users.effects.ts
@@ -1,6 +1,6 @@
 import {inject} from "@angular/core";
 import {createEffect, Actions, ofType} from "@ngrx/effects";
-import {switchMap, catchError, of} from "rxjs";
+import {switchMap, catchError, of, map} from "rxjs";
 import * as UsersActions from "./users.actions";
 import { ApiService } from "@rooi/core/http";
 
@@ -11,19 +11,20 @@ export const userEffects = createEffect(
   
       return actions$.pipe(
         ofType(UsersActions.initUsers),
-        // switchMap(
-        //   () => apiService.get<UsersDTO[]>('/users').pipe(
-        //     map(
-        //       (users) => UsersActions.loadUsersSuccess({
-        //         users: users.map(user => usersDTOAdapter.DTOtoEntity(user))
-        //       })
-        //     ),
-        //     catchError((error) => {
-        //       console.error('Error', error);
-        //       return of(UsersActions.loadUsersFailure({ error }));
-        //     })
-        //   )
-        // ),
+        switchMap(
+          () => apiService.get('/users').pipe(
+            map(
+              (users: any) => UsersActions.loadUsersSuccess({
+                users
+                // users: users.map(user => usersDTOAdapter.DTOtoEntity(user))
+              })
+            ),
+            catchError((error) => {
+              console.error('Error', error);
+              return of(UsersActions.loadUsersFailure({ error }));
+            })
+          )
+        ),
       )
     }, { functional: true }
   )
