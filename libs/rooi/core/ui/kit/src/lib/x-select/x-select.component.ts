f96f81886415975d4e492eab9fecde22924dbe10
maratreason <maratreason@mail.ru>
2024-10-11 15:58:13 +0300

    fix(rooi/ui) create select component

--- libs/rooi/core/ui/kit/src/lib/x-select/x-select.component.ts
+++ libs/rooi/core/ui/kit/src/lib/x-select/x-select.component.ts
@@ -1,12 +1,37 @@
-import {ChangeDetectionStrategy, Component} from "@angular/core";
+import {ChangeDetectionStrategy, Component, ElementRef, inject, Input} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {XOptionComponent } from "../x-option/x-option.component";
 
 @Component({
-    selector: "lib-x-select",
+    selector: "x-select",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, XOptionComponent],
     templateUrl: "./x-select.component.html",
     styleUrl: "./x-select.component.scss",
     changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {
+        "[class.is--active]": "isActive",
+        "(click)": "updateValue()",
+    },
 })
-export class XSelectComponent {}
+export class XSelectComponent {
+    private parentRef = inject(ElementRef);
+    public isActive = false;
+    
+    @Input({required: true})
+    value!: string;
+    
+    @Input({required: true})
+    options!: string[];
+    
+    public readonly classes = this.parentRef.nativeElement.getAttribute("class");
+    public currentOption = this.value;
+
+    public updateValue() {
+        this.isActive = !this.isActive;
+    }
+
+    public setOption(value: string) {
+        this.currentOption = value;
+    }
+}
